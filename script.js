abc38824df58f7914b3a0335038e9d80db2fd154
Nikhil Gupta <nikhilg7404@gmail.com>
2024-06-23 23:35:17 +0530

    initial commit

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ function scrollToSection(sectionId) {
     var section = document.getElementById(sectionId);
     section.scrollIntoView({ behavior: 'smooth' });
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    const searchIcon = document.getElementById('search-icon');
+    const searchBar = document.getElementById('search-bar');
+
+    searchIcon.addEventListener('click', function(event) {
+        event.preventDefault();
+        if (searchBar.style.display === 'none' || searchBar.style.display === '') {
+            searchBar.style.display = 'block';
+        } else {
+            searchBar.style.display = 'none';
+        }
+    });
+});
